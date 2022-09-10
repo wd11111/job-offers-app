e8f4b0374a07514ba2b6ee79ff09b3733f4c28c7
wd11111 <xxwejtixx@gmail.com>
2022-09-10 23:53:49 +0200

    implemented first unit test for httpclient

---
+++ src/main/java/com/joboffers/joboffers/infrastructure/offer/client/OfferHttpClient.java
@@ -0,0 +1,40 @@
+package com.joboffers.joboffers.infrastructure.offer.client;
+
+import com.joboffers.joboffers.infrastructure.RemoteOfferClient;
+import com.joboffers.joboffers.infrastructure.offer.dto.OfferDto;
+import lombok.AllArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
+import java.util.List;
+
+@AllArgsConstructor
+public class OfferHttpClient implements RemoteOfferClient {
+
+    private final RestTemplate restTemplate;
+    private final String uri;
+
+    @Override
+    public List<OfferDto> getOffers() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        final HttpEntity<HttpHeaders> requestEntity = new HttpEntity<>(httpHeaders);
+
+        try {
+            ResponseEntity<List<OfferDto>> response = restTemplate.exchange(uri, HttpMethod.GET, requestEntity,
+                    new ParameterizedTypeReference<List<OfferDto>>() {
+                    });
+            final List<OfferDto> body = response.getBody();
+            return (body != null) ? body : Collections.emptyList();
+        } catch (ResourceAccessException e) {
+            return Collections.emptyList();
+        }
+    }
+}
