fcb25340b9e8ec6708f53ac48dbbc7176b0d8adb
wd11111 <xxwejtixx@gmail.com>
2022-09-10 22:36:11 +0200

    Initial commit

---
+++ src/main/java/com/joboffers/joboffers/JoboffersApplication.java
@@ -0,0 +1,13 @@
+package com.joboffers.joboffers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoboffersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoboffersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joboffers/joboffers/JoboffersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joboffers.joboffers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoboffersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
